6573536c346b592f03df0f6a7792e2b5b10b81f8
Emre Demir <pi.emree@gmail.com>
2021-07-05 23:47:26 +0300

    changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("MY FISRT CLOUD RUN HELLO WORLD APP");
+  res.send("MY FISRT CLOUD RUN HELLO WORLD APP from litte home");
 });
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
